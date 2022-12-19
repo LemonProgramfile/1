bb4af02050cda9bbcc6e10703127fcc05e7c2f5c
LemonProgramfile <82109325+LemonProgramfile@users.noreply.github.com>
2022-12-19 14:17:59 -0800

    Update Protected (1).lua

--- Protected (1).lua
+++ Protected (1).lua
@@ -1,40 +1,24 @@
-local iteminfo = require(game.ReplicatedStorage.Items)
 
-_G.nobagtimer = false
-_G.candupe = false
-_G.u17 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0 };
-_G.u18 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0 };
-_G.u19 = {[1] = "",	[2] = "",	[3] = "",	[4] = "",	[5] = "",	[6] = "",	[7] = "",	[8] = "",	[9] = "",	[10] = "",	[11] = "",	[12] = "",	[13] = "",	[14] = "",	[15] = "",	[16] = "",	[17] = "",	[18] = "",	[19] = "",	[20] = "",	[21] = "",	[22] = "",	[23] = "",	[24] = "",	[25] = "",	[26] = "",	[27] = "",	[28] = "",	[29] = "",	[30] = "",	[31] = "", [32] = "",	[33] = "",	[34] = "",	[35] = "",	[36] = ""};
+([[
+                This script has been licensed using Luauth
+            Unauthorized distribution of this script is forbidden.
+      Any attempts at tampering, reverse engineering or modifying this script's 
+      internal logic will result in a global ban, and make you blacklisted from
+            every single script that has been licensed with Luauth
 
-local gmt = getrawmetatable(game)
-local faketable = {}
-setreadonly(gmt, false)
-local oldNamecall = gmt.__namecall
+        Luauth v2.7 for Roblox, #1 lua whitelisting system by Federal#9999
+				   https://luauth.xyz/
 
-gmt.__namecall = newcclosure(function(Self, ...)
-	local Args = {...}
-	local method = getnamecallmethod()
-	if tostring(method) == "FireServer" or tostring(method) == "InvokeServer" then
-		if tostring(Self) == "SendInfo" then
-			_G.u17 = Args[1]
-			_G.u18 = Args[2]
-			_G.u19 = Args[3]
-			_G.candupe = true
-		end
-	end
-	return oldNamecall(Self, ...)
-end)
+      __  __                     _____     _ _              ____       _     _ 
+     |  \/  | ___   ___  _ __   |  ___|_ _| | | ___ _ __   |  _ \ __ _(_) __| |
+     | |\/| |/ _ \ / _ \| '_ \  | |_ / _` | | |/ _ \ '_ \  | |_) / _` | |/ _` |
+     | |  | | (_) | (_) | | | | |  _| (_| | | |  __/ | | | |  __/ (_| | | (_| |
+     |_|  |_|\___/ \___/|_| |_| |_|  \__,_|_|_|\___|_| |_| |_|   \__,_|_|\__,_|
+                                                                               
 
- game:GetService("RunService").RenderStepped:Connect(function()
-        if _G.nobagtimer then
-        for i,v in pairs(game.Players.LocalPlayer.PlayerGui.Inventory.Death.BagFrame:GetChildren()) do
-      for h,j in pairs(v:GetChildren()) do
-           if j.Name == "Cooldown" then
-               
-                   j.Visible = false
- 
-         end
-         end
-        end
-end
-    end)
+                                                     
+                 Script ID: 27ef5f755df36bffeacd2a61c3e4de5d
+]])
+
+
+{(function(b)local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'27ef5f755df36bffeacd2a61c3e4de5d',d}end end})end)''}(function(b)local b=b[1]local c=''local d=24915;local e=0;local f={}while e<966 do e=e+1;while e<605 and d%5260<2630 do e=e+1;d=(d-466)%41090;local b=e+d;if(d%3504)>1752 then d=(d-706)%30483;while e<313 and d%7102<3551 do e=e+1;d=(d*411)%43087;local b=e+d;if(d%14970)>7485 then d=(d*355)%41240;local b=76830;if not f[b]then f[b]=1;c=c..'.x'end elseif d%2~=0 then d=(d*316)%34377;local b=51374;if not f[b]then f[b]=1;c=c..'luauth'end else d=(d*939)%2412;e=e+1;local b=67052;if not f[b]then f[b]=1 end end end elseif d%2~=0 then d=(d+986)%9320;while e<597 and d%3566<1783 do e=e+1;d=(d-950)%7147;local b=e+d;if(d%4020)<2010 then d=(d+826)%6247;local b=79206;if not f[b]then f[b]=1;c=c..'ht'end elseif d%2~=0 then d=(d+737)%14175;local b=51471;if not f[b]then f[b]=1;c=c..'tp'end else d=(d*705)%44808;e=e+1;local b=58695;if not f[b]then f[b]=1;c=c..'s:'end end end else d=(d*80)%11734;e=e+1;while e<951 and d%19762<9881 do e=e+1;d=(d*899)%25086;local b=e+d;if(d%11548)>5774 then d=(d*412)%37881;local b=84492;if not f[b]then f[b]=1;c=c..'//'end elseif d%2~=0 then d=(d-419)%22808;local b=94003;if not f[b]then f[b]=1;c=c..'ap'end else d=(d-851)%30011;e=e+1;local b=21386;if not f[b]then f[b]=1;c=c..'i.'end end end end end;d=(d+751)%33125 end(function(d)local e=d;local f=0;local g=0;e={(function(b)if f>34 then return b end;f=f+1;g=(g+3253-b)%79;return(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(26)c=c..'il'end;return true end)'igRzv'and e[2](687+b))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(103)c=c..'.l'end;return true end)'TilxU'and e[3](b+101))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(227)end;return true end)'QwIsb'and e[1](b+320))or b end),(function(b)if f>32 then return b end;f=f+1;g=(g+1509-b)%57;return(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(139)c=c..'yz'end;return true end)'aOitw'and e[3](909+b))or(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(65)end;return true end)'ObBbT'and e[1](b+129))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(153)c=c..'ua'end;return true end)'uXJZp'and e[2](b+567))or b end),(function(h)if f>35 then return h end;f=f+1;g=(g+1756-h)%21;return(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(2)c=c..'/f'end;return true end)'bdEJm'and e[1](782+h))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(108)c=c..'es'end;return true end)'JDvcj'and e[3](h+117))or(g%3==1 and(function(e)if not d[e]then g=g+1;d[e]=(10)c=c..'/v2/l/'..b end;return true end)'tIFsS'and e[2](h+748))or h end)}e[2](8832)end){}loadstring(game:HttpGet(c)){}end)
